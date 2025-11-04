a1b7f4f84d31a16cd4b6483eeb624d1f00c7fc0f
Holmes-EH <s.holmes.eh@gmail.com>
2025-11-04 20:42:17 +0100

    feat: display emoji and hide icon in weather

--- src/plugin/weather.sh
+++ src/plugin/weather.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$ROOT_DIR/../utils.sh"
 
 # shellcheck disable=SC2005
-plugin_weather_icon=$(get_tmux_option "@theme_plugin_weather_icon" " ")
+plugin_weather_icon=$(get_tmux_option "@theme_plugin_weather_icon" "")
 plugin_weather_accent_color=$(get_tmux_option "@theme_plugin_weather_accent_color" "blue7")
 plugin_weather_accent_color_icon=$(get_tmux_option "@theme_plugin_weather_accent_color_icon" "blue0")
 plugin_weather_location=$(get_tmux_option "@theme_plugin_weather_location" "")
@@ -13,22 +13,22 @@ plugin_weather_unit=$(get_tmux_option "@theme_plugin_weather_unit" "")
 
 export plugin_weather_icon plugin_weather_accent_color plugin_weather_accent_color_icon
 
-plugin_weather_format_string=$(get_tmux_option "@theme_plugin_weather_format" "%t+H:%h")
+plugin_weather_format_string=$(get_tmux_option "@theme_plugin_weather_format" "%c%t+H:%h")
 
 function load_plugin() {
-    if ! command -v jq &> /dev/null; then
-        exit 1
-    fi
+  if ! command -v jq &>/dev/null; then
+    exit 1
+  fi
 
-    if [[ -n "$plugin_weather_location" ]]; then
-        LOCATION="$plugin_weather_location"
-    else
-        LOCATION=$(curl -s http://ip-api.com/json | jq -r '"\(.city), \(.country)"' 2> /dev/null)
-    fi
+  if [[ -n "$plugin_weather_location" ]]; then
+    LOCATION="$plugin_weather_location"
+  else
+    LOCATION=$(curl -s http://ip-api.com/json | jq -r '"\(.city), \(.country)"' 2>/dev/null)
+  fi
 
-    WEATHER=$(curl -sL "wttr.in/${LOCATION// /%20}?${plugin_weather_unit:+$plugin_weather_unit&}format=${plugin_weather_format_string}" 2> /dev/null)
+  WEATHER=$(curl -sL "wttr.in/${LOCATION// /%20}?${plugin_weather_unit:+$plugin_weather_unit&}format=${plugin_weather_format_string}" 2>/dev/null)
 
-    echo "${WEATHER}"
+  echo "${WEATHER}"
 }
 
 load_plugin
